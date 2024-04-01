299f45fe5e7ba0a97374c79731de0384b62ebce3
AiDinaAgustin <aidinagustin2@gmail.com>
2024-04-01 09:38:48 +0700

    faq question

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,4 +43,16 @@ function validateForm() {
     var encodedPesan = encodeURIComponent(templatePesan);
     window.location.href = 'https://wa.me/' + nomorWhatsApp + '?text=' + encodedPesan;
   }
+
+    //   Dina
+  $(document).ready(function () {
+    $("#faq-store").click(function () {
+      $(".accordion-store").show();
+      $(".accordion-vape").hide();
+    });
+    $("#faq-vape").click(function () {
+      $(".accordion-store").hide();
+      $(".accordion-vape").show();
+    });
+  });
   
